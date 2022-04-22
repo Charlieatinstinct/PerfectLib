ecb73fd4d3a200eb1a960017d783d5ec884cb138
Charlieatinstinct <charlieatinstinct@163.com>
2022-04-22 19:03:58 +0800

    init package

--- Package.swift
+++ Package.swift
@@ -1,28 +1,47 @@
-// swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:4.1
+//
+//  Package.swift
+//  PerfectLib
+//
+//  Created by Kyle Jessup on 3/22/16.
+//	Copyright (C) 2016 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
 
 import PackageDescription
 
+#if os(Linux)
 let package = Package(
-    name: "PerfectLib",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "PerfectLib",
-            targets: ["PerfectLib"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "PerfectLib",
-            dependencies: []),
-        .testTarget(
-            name: "PerfectLibTests",
-            dependencies: ["PerfectLib"]),
-    ]
+	name: "PerfectLib",
+	products: [
+		.library(name: "PerfectLib", targets: ["PerfectLib"])
+	],
+	dependencies: [.package(url: "https://github.com/PerfectlySoft/Perfect-LinuxBridge.git", from: "3.0.0")],
+	targets: [
+		.target(name: "PerfectLib", dependencies: ["LinuxBridge"]),
+		.testTarget(name: "PerfectLibTests", dependencies: ["PerfectLib"])
+	]
 )
+#else
+let package = Package(
+	name: "PerfectLib",
+	products: [
+		.library(name: "PerfectLib", targets: ["PerfectLib"])
+	],
+	dependencies: [],
+	targets: [
+		.target(name: "PerfectLib", dependencies: []),
+		.testTarget(name: "PerfectLibTests", dependencies: ["PerfectLib"])
+	]
+)
+#endif
--- Sources/PerfectLib/PerfectLib.swift
+++
@@ -1,6 +0,0 @@
-public struct PerfectLib {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
